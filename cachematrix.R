43c8f84ccc529c26bf2e3f98a43963f28d6641d7
Eva Espejo <evaespejo1808@gmail.com>
2016-01-22 19:14:10 +0100

    my quizz.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,28 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) { 
+    m <<-NULL
+        setmatrix <- function (y){
+            x <<- y
+            m <<- NULL
+}
+        getmatrix <- function()  x
+        setsolve <- function (solve) m<<-solve
+        getsolve <- function ()  m 
+        list (setmatrix = setmatrix, getmatrix = getmatrix,setsolve = setsolve, getsolve = getsolve)
+
 
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x=matrix(),...) {
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached matrix")
+        return(m)
+    }
         ## Return a matrix that is the inverse of 'x'
 }
